a548b47485d7b096e7002c5be1eeba81ef93e789
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-06-29 11:17:55 +0300

    string

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+console.log(excitedWelcomeMessage);
+const currentUser = 'Grace Hopper';
+
+...
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 2)}!`;
